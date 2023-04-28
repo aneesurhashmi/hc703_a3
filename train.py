9d8dc03f8e849ba89ffef52d1d415a9f4e9d852b
Anees Hashmi <aneesurhashmi@gmail.com>
2023-04-29 02:58:57 +0400

    Khatam Shud

--- train.py
+++ train.py
@@ -26,7 +26,6 @@ def main(args):
     # Tensorboard
     writer = tensorboardX.SummaryWriter(os.path.join(args.log_dir,  args.model_name))
 
-    # model = UNet(n_channels=3, n_classes=args.num_classes) # inlcluding background
     if args.pretrained:
         model = smp.Unet(
             encoder_name=args.backbone,        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
@@ -42,31 +41,15 @@ def main(args):
         )
     model = model.to(args.device)
 
-    # Define the loss function and optimizer
-    # criterion = multi_class_dice_loss
-    # criterion_bce = nn.BCEWithLogitsLoss()
-    # criterion_l2 = nn.MSELoss()
-    # criterion_dice = monai.losses.DiceFocalLoss()
-    # criterion_ce = nn.CrossEntropyLoss()
-
-    if args.loss == "dice":
-        criterion = monai.losses.DiceLoss() # 1 - dice_score
-    elif args.loss == "focal":
-        criterion = monai.losses.FocalLoss()
-    elif args.loss == "GeneralizedDiceFocalLoss":
-        criterion = monai.losses.GeneralizedDiceFocalLoss()
-    elif args.loss == "BCE":
+    if args.loss == "dice": # 1 - dice_score
+        criterion = monai.losses.DiceLoss() 
+    elif args.loss == "BCE": # binary cross entropy
         criterion = nn.BCELoss()
-
-    elif args.loss == "dice_score": # returns 1 - dice_score with for non-empty values only
+    elif args.loss == "dicebce": # combined loss
+        bce_loss = nn.BCELoss()
+        dice_loss = monai.losses.DiceLoss()
         def criterion(y_pred, y_true):
-            if len(y_pred.shape) == 3: # for single class
-                return monai.metrics.compute_dice(y_pred.reshape(y_pred.shape[0], 1, args.image_size, args.image_size), 
-                                            y_true.reshape(y_true.shape[0], 1, args.image_size, args.image_size), 
-                                            include_background=False, ignore_empty=False).mean().item()
-            # for multi class
-            return monai.metrics.compute_dice(y_pred, y_true, include_background=False, ignore_empty=False).mean().item()
-
+            return bce_loss(y_pred, y_true) + dice_loss(y_pred, y_true)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
@@ -78,10 +61,6 @@ def main(args):
         total_train_loss_cache = [] # used for tensorboard logs
         cls1_train_loss_cache = []
         cls2_train_loss_cache = []
-
-        # l2_train_loss_cache = []
-        # bce_train_loss_cache = []
-        # dicefocal_train_loss_cache = []
 
         model.train()
         for i, (images, true_masks) in tqdm(enumerate(train_loader)):
@@ -103,8 +82,6 @@ def main(args):
             # weighted loss
             loss = loss_cl1*args.loss_weight_cls1 + loss_cl2*args.loss_weight_cls2 
 
-            # loss = criterion(pred_masks, true_masks)
-            
             # Backward and optimize
             loss.backward()
             optimizer.step()
@@ -128,7 +105,6 @@ def main(args):
             val_loss_cls1 = 0
             val_loss_cls2 = 0
             
-
             for images, true_masks in val_loader:
                 images = images.to(args.device)
                 true_masks = true_masks.to(args.device)
@@ -151,14 +127,6 @@ def main(args):
                 val_jaccard_score_cls1 += monai.metrics.compute_iou(pred_cls1.reshape(pred_cls1.shape[0], 1, 256, 256), true_cls1.reshape(true_cls1.shape[0], 1, 256, 256), include_background=False, ignore_empty=False).mean().item()
                 val_jaccard_score_cls2 += monai.metrics.compute_iou(pred_cls2.reshape(pred_cls1.shape[0], 1, 256, 256), true_cls2.reshape(true_cls1.shape[0], 1, 256, 256), include_background=False, ignore_empty=False).mean().item()
 
-                # print(monai.metrics.compute_dice(pred_masks, true_masks, include_background=False).mean())
-                # print(monai.metrics.compute_dice(pred_cls1.reshape(pred_cls1.shape[0], 1, 256, 256), true_cls1.reshape(true_cls1.shape[0], 1, 256, 256), include_background=False).mean())
-                # print(monai.metrics.compute_dice(pred_cls2.reshape(pred_cls1.shape[0], 1, 256, 256), true_cls2.reshape(true_cls1.shape[0], 1, 256, 256), include_background=False).mean())
-
-                # print(monai.metrics.compute_iou(pred_masks, true_masks, include_background=False).mean())
-                # print(monai.metrics.compute_iou(pred_cls1.reshape(pred_cls1.shape[0], 1, 256, 256), true_cls1.reshape(true_cls1.shape[0], 1, 256, 256), include_background=False).mean())
-                # print(monai.metrics.compute_iou(pred_cls2.reshape(pred_cls1.shape[0], 1, 256, 256), true_cls2.reshape(true_cls1.shape[0], 1, 256, 256), include_background=False).mean())
-
             print(len(val_dataset))
             val_loss_cls1 /= len(val_dataset)
             val_loss_cls2 /= len(val_dataset)
@@ -172,10 +140,7 @@ def main(args):
             val_jaccard_score_cls1 /= len(val_dataset)
             val_jaccard_score_cls2 /= len(val_dataset)
 
-            # val_loss_bce /= len(val_dataset)
-
             #  Log to tensorboard
-            # writer.add_scalar('val_loss_dice', val_dice_score, epoch)
             writer.add_scalar('vall_loss_cls1', val_loss_cls1, epoch)
             writer.add_scalar('vall_loss_cls2', val_loss_cls2, epoch)
             # log dice score
@@ -200,7 +165,6 @@ def main(args):
             print(f"val jaccard score: {val_jaccard_score},val jaccard score cls1: {val_jaccard_score_cls1} , val jaccard score cls2: {val_jaccard_score_cls2}")
             print("------------------------------------")
             print('')
-
 
             if val_dice_score > best_eval_dice:
                 best_eval_dice = val_dice_score
@@ -229,7 +193,7 @@ if __name__ == '__main__':
     args.add_argument('--lr', type=float, default=1e-5)
     args.add_argument('--log_step', type=int, default=10)
     args.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
-    args.add_argument('--num_workers', type=int, default=24)
+    args.add_argument('--num_workers', type=int, default=32)
     args.add_argument('--image_size', type=int, default=256)
     
     # loss args
@@ -246,6 +210,8 @@ if __name__ == '__main__':
     print(config)
     main(config)
 
-    # experiment 1: unet_dice_loss_mobilenet_v2_1.0_5.0 (not good enough)
-    # experiment 2: unet_dice_loss_mobilenet_v2_1.0_1.0 
-
+    # experiment 1: unet_dice_loss_mobilenet_v2_1.0_1.0 (not good enough)
+    # experiment 2: unet_dice_loss_mobilenet_v2_1.0_5.0 (better)
+    # experiment 3: unet_bce_loss_mobilenet_v2_1.0_5.0 (bettere)
+    # experiment 4: unet_dice_loss_resnet101_1.0_5.0 (not good enough)
+    # experiment 5: unet_dicebce_loss_resnet101_1.0_5.0 (best)
